4af04a8165c33ea961b1355c206543c58ff6eedc
gngrninja <thegngrninja@gmail.com>
2019-08-18 11:26:12 -0700

    adding ability to change playing

--- src/SevenThree/Modules/Admin/Admin.cs
+++ src/SevenThree/Modules/Admin/Admin.cs
@@ -5,18 +5,29 @@ using Discord;
 using Discord.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using SevenThree.Database;
+using Discord;
+using Discord.WebSocket;
 
 namespace SevenThree.Modules
 {
     public class Admin : ModuleBase
     {
         private readonly SevenThreeContext _db;
+        private readonly DiscordSocketClient _client;
 
         public Admin(IServiceProvider services)
         {
-            _db =  services.GetRequiredService<SevenThreeContext>();
+            _db     = services.GetRequiredService<SevenThreeContext>();
+            _client = services.GetRequiredService<DiscordSocketClient>();
         }
         
+        [Command("playing")]
+        [RequireOwner]
+        public async Task ChangePlaying([Remainder] string args)
+        {
+            await _client.SetGameAsync(args);
+        }
+
         [Command("prefix",RunMode = RunMode.Async)]
         [RequireUserPermission(GuildPermission.KickMembers)]
         public async Task ChangePrefix(char prefix)
