83b99923e37fe6e6b9ffc65f999873ba1ad99dca
gngrninja <thegngrninja@gmail.com>
2019-08-18 19:14:51 -0700

    adding reacting handling as async

--- src/SevenThree/Services/ReactionService.cs
+++ src/SevenThree/Services/ReactionService.cs
@@ -23,60 +23,68 @@ namespace SevenThree.Services
             _client.ReactionRemoved += ReactionRemoved;
         }
 
-        private async Task ReactionRemoved(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
+        private Task ReactionRemoved(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
         {
-            var guild  = _client.Guilds.Where(g => g.Id == 611634254438465537).FirstOrDefault() as IGuild;            
-            var emotes = guild.Emotes;
-            var roles  = guild.Roles; 
-            if (arg1.Id == 612768518152388648)
-            {
-                if (arg3.Emote.Name == "ham")
-                {                                       
-                    if (!arg3.User.Value.IsBot)
-                    {
-                        var user = arg3.User.Value as IGuildUser;
-                        await user.RemoveRoleAsync(roles.Where(r => r.Name == "ham").FirstOrDefault());
+            var _ = Task.Run(async () => 
+            {   
+                var guild  = _client.Guilds.Where(g => g.Id == 611634254438465537).FirstOrDefault() as IGuild;            
+                var emotes = guild.Emotes;
+                var roles  = guild.Roles; 
+                if (arg1.Id == 612768518152388648)
+                {
+                    if (arg3.Emote.Name == "ham")
+                    {                                       
+                        if (!arg3.User.Value.IsBot)
+                        {
+                            var user = arg3.User.Value as IGuildUser;
+                            await user.RemoveRoleAsync(roles.Where(r => r.Name == "ham").FirstOrDefault());
+                        }
                     }
-                }
-                else if (arg3.Emote.Name == "ham2be")
-                {                    
-                    if (!arg3.User.Value.IsBot)
-                    {
-                        var user = arg3.User.Value as IGuildUser;
-                        await user.RemoveRoleAsync(roles.Where(r => r.Name == "ham2be").FirstOrDefault());
-                    }                    
-                }
-            }
+                    else if (arg3.Emote.Name == "ham2be")
+                    {                    
+                        if (!arg3.User.Value.IsBot)
+                        {
+                            var user = arg3.User.Value as IGuildUser;
+                            await user.RemoveRoleAsync(roles.Where(r => r.Name == "ham2be").FirstOrDefault());
+                        }                    
+                    }
+                }                          
+            });
+            return Task.CompletedTask;
         }
 
-        private async Task ReactionAdded(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
+        private Task ReactionAdded(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
         {
-            var guild  = _client.Guilds.Where(g => g.Id == 611634254438465537).FirstOrDefault() as IGuild;            
-            var emotes = guild.Emotes;
-            var roles  = guild.Roles; 
-            if (arg1.Id == 612768518152388648)
+            var _ = Task.Run(async () =>
             {
-                if (arg3.Emote.Name == "ham")                
-                {                    
-                    System.Console.WriteLine("ham");
-                    if (!arg3.User.Value.IsBot)
-                    {
-                        var user = arg3.User.Value as IGuildUser;
-                        await user.AddRoleAsync(roles.Where(r => r.Name == "ham").FirstOrDefault());
-                        await user.RemoveRoleAsync(roles.Where(r => r.Name == "ham2be").FirstOrDefault());
+                var guild  = _client.Guilds.Where(g => g.Id == 611634254438465537).FirstOrDefault() as IGuild;            
+                var emotes = guild.Emotes;
+                var roles  = guild.Roles; 
+                if (arg1.Id == 612768518152388648)
+                {
+                    if (arg3.Emote.Name == "ham")                
+                    {                    
+                        System.Console.WriteLine("ham");
+                        if (!arg3.User.Value.IsBot)
+                        {
+                            var user = arg3.User.Value as IGuildUser;
+                            await user.AddRoleAsync(roles.Where(r => r.Name == "ham").FirstOrDefault());
+                            await user.RemoveRoleAsync(roles.Where(r => r.Name == "ham2be").FirstOrDefault());
+                        }
+                    }
+                    else if (arg3.Emote.Name == "ham2be")
+                    {                    
+                        System.Console.WriteLine("ham2be");
+                        if (!arg3.User.Value.IsBot)
+                        {
+                            var user = arg3.User.Value as IGuildUser;
+                            await user.AddRoleAsync(roles.Where(r => r.Name == "ham2be").FirstOrDefault());
+                            await user.RemoveRoleAsync(roles.Where(r => r.Name == "ham").FirstOrDefault());
+                        }                    
                     }
                 }
-                else if (arg3.Emote.Name == "ham2be")
-                {                    
-                    System.Console.WriteLine("ham2be");
-                    if (!arg3.User.Value.IsBot)
-                    {
-                        var user = arg3.User.Value as IGuildUser;
-                        await user.AddRoleAsync(roles.Where(r => r.Name == "ham2be").FirstOrDefault());
-                        await user.RemoveRoleAsync(roles.Where(r => r.Name == "ham").FirstOrDefault());
-                    }                    
-                }
-            }
+            });
+            return Task.CompletedTask;
         }
     }
 }
